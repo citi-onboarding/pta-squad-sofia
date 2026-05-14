609d3590158bac7870c8f9bb7a78a0344291af2d
Guilherme Montenegro <guilhermeu028@gmail.com>
2026-05-13 21:50:03 -0300

    feature/bookCardComponent: created reusable BookCard component and added mocked data

---
+++ client/src/components/bookcard/index.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Eye, Trash2, Bookmark } from 'lucide-react';
+
+interface BookCardProps {
+  title: string;
+  author: string;
+  category: string;
+  stock: number;
+  imageUrl: string;
+}
+
+
+export const BOOKS_MOCK: BookCardProps[] = [
+  {
+    title: "Clean Code",
+    author: "Robert C. Martin",
+    category: "Tecnologia",
+    stock: 5,
+    imageUrl: "/images/Tecnologia.png"
+  },
+  {
+    title: "O Pequeno Príncipe",
+    author: "Antoine de Saint-Exupéry",
+    category: "Infantil",
+    stock: 8,
+    imageUrl: "images/Infantil.png"
+  },
+  {
+    title: "Dom Casmurro",
+    author: "Machado de Assis",
+    category: "Romance",
+    stock: 3,
+    imageUrl: "/images/Romance.png"
+  }, 
+  {
+    title: "Sapiens",
+    author: "Yuval Noah Harari",
+    category: "História",
+    stock: 6,
+    imageUrl: "images/Historia.png"
+  },
+  {
+    title: "Cosmos",
+    author: "Carl Sagan",
+    category: "Ciências",
+    stock: 4,
+    imageUrl: "images/Ciencias.png"
+  },
+  {
+    title: "1984",
+    author: "George Orwell",
+    category: "Romance",
+    stock: 7,
+    imageUrl: "images/Romance.png"
+  }
+];
+
+export const BookCard = ({ title, author, category, stock, imageUrl }: BookCardProps) => {
+    return (
+      <article className="flex flex-col gap-6  w-98.5 p-4 bg-white shadow-lg rounded-lg">
+          {/* Imagem do livro */}
+          <img 
+            className="w-full h-64 object-cover rounded-md bg-gray-200"
+            src={imageUrl}
+            alt={`Capa do livro ${title}`}
+           /> 
+
+           {/* Informações do livro */}
+          <div className="flex flex-col gap-2">
+            <h2 className="text-[rgba(30,30,30,1)] text-lg font-medium leading-6.75">{title}</h2>
+            <p className="text-[rgba(113,113,130,1)] text-base leading-6">{author}</p>
+            <p className="text-[rgba(0,195,137,1)] text-sm font-bold leading-5">{category}</p>
+            <p className="text-[rgba(30,30,30,1)] text-sm leading-5">
+              <span className="font-medium">Disponível: </span> {stock} unidade(s)
+            </p>
+          </div>
+
+          {/* Botões */}
+        <div className="grid grid-cols-[115px_147px_66px] gap-2">
+          <button className="flex items-center justify-center gap-[7px] w-28.75 h-[47.33px] bg-transparent border-[1.67px] border-solid border-[rgba(0,195,137,1)] rounded-lg cursor-pointer">  
+            <Eye  size={18} className="text-[rgba(0,195,137,1)]" />
+            <span className="font-medium text-[rgba(0,195,137,1)] text-base leading-6">Ver</span>
+          </button>
+          <button className="flex items-center gap-[6px] justify-center w-36.75 h-[47.33px] bg-[rgba(0,195,137,1)] rounded-lg cursor-pointer">
+            <Bookmark size={18} className="text-[rgba(255,255,255,1)]" />
+            <span className="font-medium text-[rgba(255,255,255,1)] text-base leading-6">Emprestar</span>
+          </button>
+          <button className="flex items-center justify-center  w-16.5 h-[47.33px] bg-[rgba(239,68,68,1)] rounded-lg cursor-pointer">
+            <Trash2 size={18} className="text-[rgba(255,255,255,1)]" />
+          </button>
+        </div>
+      </article>
+    );
+  }
+  
+  
