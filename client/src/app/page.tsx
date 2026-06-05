672f859561b26ac47ca488f3f9988341c1c3a71b
luismiguuel <sirmiguuel@gmail.com>
2026-06-05 18:54:31 -0300

    fix(adjustBookCardSize): change in bookcard size

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
 
           {/* Grid Cards*/}
           {filteredBooks.length > 0 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
               {filteredBooks.map((book) => (
                 <BookCard 
                   key={book.id}
